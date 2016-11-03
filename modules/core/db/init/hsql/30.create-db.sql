738c6b019ca1ac9927f8369bda88c20ef475ff9b
zlatoverov <zlatoverov@haulmont.com>
2016-11-03 18:24:20 +0400

    Add data for customer

--- modules/core/db/init/hsql/30.create-db.sql
+++ modules/core/db/init/hsql/30.create-db.sql
@@ -1,1 +1,106 @@
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 2, '2016-10-30 15:55:18', 'admin', '2016-11-03 18:08:35', 'admin', null, null,
+'Antionette Cheyne', 0, 'Hong Kong', 'China', 'a405db59-e674-4f63-8afe-269dda788fe8')^
 
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Georgette Nanez', 10, 'Bangalore', 'India', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Bennie	Lawson', 20, 'Bangkok', 'Thailand', 'a405db59-e674-4f63-8afe-269dda788fe8')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Elijah	Simpson', 30, 'Beijing', 'China', '60885987-1b61-4247-94c7-dff348347f93')^
+
+--
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Katherine	Norton', 0, 'Jeddah', 'Saudi Arabia', 'a405db59-e674-4f63-8afe-269dda788fe8')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Debbie	Kim', 10, 'Bogota', 'Colombia', 'a405db59-e674-4f63-8afe-269dda788fe8')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Daryl	Young', 20, 'Istanbul', 'Turkey', 'a405db59-e674-4f63-8afe-269dda788fe8')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Marlene	Russell', 30, 'Dongguan', 'China', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Louis	Stewart', 0, 'Ahmedabad', 'India', 'a405db59-e674-4f63-8afe-269dda788fe8')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Horace	Curry', 10, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Gabriel	Bell', 20, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Guadalupe	Duncan', 30, 'Moscow', 'Russia', 'a405db59-e674-4f63-8afe-269dda788fe8')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Salvador	Mendez', 0, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Roman	Sutton', 10, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Manuel	Wallace', 20, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Charles	Mann', 30, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'June	Henderson', 0, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Cory	Drake', 10, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Travis	Barnett', 20, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Erica	Goodman', 30, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
+
+insert into NE_CUSTOMER
+(ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, NAME, STATUS, ADDRESS_CITY, ADDRESS_COUNTRY, USER_ID)
+values (newid(), 8, '2016-10-28 17:36:09', 'admin', '2016-11-02 18:29:23', 'admin', null, null,
+'Ebony	Stone', 0, 'Moscow', 'Russia', '60885987-1b61-4247-94c7-dff348347f93')^
